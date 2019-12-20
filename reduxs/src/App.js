5eb82823e2676df18c806504f80a9ffbcb04e3fa
Treek <nbatre1@yahoo.com>
2019-12-20 14:13:33 -0600

    keep moving forward

--- reduxs/src/App.js
+++ reduxs/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import FrozenDept from './Components/FrozenDepartment';
 import ProduceDept from './Components/ProducerDepartment';
 import MeatProduceDept from './Components/MeatDepartment';
+import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
